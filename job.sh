cb85e3568f6d69f5749d78489c1d93fb4de55a15
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2021-05-04 15:26:28 +0200

    compile source in job and copy to mount in container

--- job.sh
+++ job.sh
@@ -14,21 +14,16 @@ echo "Process:" $PROCESS
 FILE=$3
 echo "File:" $FILE
 
-# NOTE: Start the OUTPUT_DIR path with root:// to copy via XRootD, e.g at CERN
-# root://eosuser.cern.ch//eos/user/FIRST_LETTER/USERNAME/some_folder
-# NOTE: If the output directory is local, it should be mounted in the docker container.
+if [[ ${FILE} == *"Run2012"* ]]; then
+    CONFIG=configs/data_cfg.py
+else
+    CONFIG=configs/simulation_cfg.py
+fi
+
+echo "CMSSW config:" $CONFIG
+
 OUTPUT_DIR=/path/to/output/dir
 echo "Output directory:" $OUTPUT_DIR
-
-CMSSW_BASE=/home/cmsusr/CMSSW_5_3_32/src
-echo "CMSSW base:" $CMSSW_BASE
-
-if [[ ${FILE} == *"Run2012"* ]]; then
-    CONFIG=${CMSSW_BASE}/src/workspace/AOD2NanoAOD/configs/data_cfg.py
-else
-    CONFIG=${CMSSW_BASE}/src/workspace/AOD2NanoAOD/configs/simulation_cfg.py
-fi
-echo "CMSSW config:" $CONFIG
 
 echo "Hostname:" `hostname`
 
@@ -43,12 +38,16 @@ echo "### Start working"
 # Make output directory
 mkdir -p ${OUTPUT_DIR}/${PROCESS}
 
+
 # Setup CMSSW
-cd $CMSSW_BASE
+echo "Setup CMSSW"
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-eval `scramv1 runtime -sh`
+scramv1 project CMSSW CMSSW_5_3_32 # cmsrel CMSSW_5_3_32
+cd CMSSW_5_3_32
+eval `scramv1 runtime -sh` # cmsenv
 
 # Clone and build the repo
+echo "Clone and build the repository"
 mkdir workspace
 cd workspace
 git clone git://github.com/stwunsch/AOD2NanoAODOutreachTool -b dockerjobs AOD2NanoAOD --depth 1
@@ -66,7 +65,7 @@ sed -i -e 's,^files.extend,#files.extend,g' $CONFIG_COPY
 # Modify CMSSW config to read lumi mask from EOS
 sed -i -e 's,data/Cert,'${CMSSW_BASE}'/src/workspace/AOD2NanoAOD/data/Cert,g' $CONFIG_COPY
 
-# Modify config to write output directly to output directory
+# Modify config to write output directly to output file
 sed -i -e 's,output.root,'${PROCESS}_${ID}.root',g' $CONFIG_COPY
 
 # Print config
@@ -77,7 +76,8 @@ cmsRun $CONFIG_COPY
 
 # Copy output file
 # NOTE: If the path is a local path, it just copies the file
-xrdcp -f ${PROCESS}_${ID}.root ${OUTPUT_DIR}/${PROCESS}/${PROCESS}_${ID}.root
+cp ${PROCESS}_${ID}.root ${OUTPUT_DIR}/${PROCESS}/${PROCESS}_${ID}.root
 rm ${PROCESS}_${ID}.root
 
 echo "### End of job"
+
